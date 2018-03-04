46fb5343c16325cd8bf4c31d3c9cf23a2850aaeb
Colm Coughlan <colmcoughlanirl@gmail.com>
2018-03-04 23:16:18 +0000

    Add blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "https://astronomicalproblems.blogspot.com/feeds/posts/default?alt=rss",
+        "http://blog.colmcoughlan.com//feeds/posts/default?alt=rss",
         
         {
         // how many entries do you want?
